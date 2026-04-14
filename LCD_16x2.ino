cc305675da829155127b32e0ed5ec95ba6aa7071
Ahmed Badrawi <ahmed.badrawi@outlook.de>
2026-04-14 18:50:59 +0200

    Create LCD_16x2.ino

---
+++ LCD_16x2.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+    
+  lcd.setCursor(1, 0);
+  lcd.print(" Loading...");
+  delay(2000);
+  lcd.clear();
+  
+}
+
+void loop() {
+    lcd.setCursor(0, 0);
+    lcd.print("Welcome to");
+    lcd.setCursor(0, 1);
+    lcd.print("Ahmed Project");
+    
+  
+}
